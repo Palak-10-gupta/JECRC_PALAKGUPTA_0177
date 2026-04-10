7b78188af146a13f64c44c4b7fe0b7f1e0c918f2
Palak Gupta <your_github_email@example.com>
2026-04-10 20:48:15 +0530

    Added assessment-01 GroceryPortal

---
+++ Week-09/10-04-2026/assessment-01/GroceryPortal/Models/attributes/QuantityAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GroceryPortal.Models.attributes
+{
+    public class QuantityAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            // ✅ Allow null (important for PATCH)
+            if (value == null)
+                return ValidationResult.Success;
+
+            int quantity = (int)value;
+
+            if (quantity <= 0)
+                return new ValidationResult("Quantity must be greater than 0");
+
+            return ValidationResult.Success;
+        }
+    }
+}
