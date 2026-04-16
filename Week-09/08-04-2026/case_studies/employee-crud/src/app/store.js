ba710e39547c209ba67c354143764558199e70c7
Palak Gupta <your_github_email@example.com>
2026-04-16 18:49:17 +0530

    Restructured folder

--- Week-09/08-04-2026/case_studies/employee-crud/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import employeeReducer from "../features/employees/employeeSlice";
-
-export const store = configureStore({
-  reducer: {
-    employees: employeeReducer,
-  },
-});
